0ced96edf7648885b7b9a1d6c9675b68dc52dbdf
HARI <ssharikrishnan8@gmail.com>
2024-06-20 17:07:30 +0530

    updated the content div styling

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,9 +5,8 @@ function Content(){
     return (
         <div className="container">
             <div className="box left">
-                NETWORK ANALYSIS & INTRUSION DETECTION SYSTEM
+                <span id="name">NETWORK ANALYSIS & INTRUSION DETECTION SYSTEM</span>
             </div>
-            <div className="box middle"></div>
             <div className="box right">
                 <button class="custom-btn btn">Start Live Data Capture</button>
                 <button class="custom-btn btn">Custom Check</button>
